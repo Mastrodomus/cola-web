cd5d2bef4a4daff54d9acafcfd3712194c71befe
Mastrodomus <santigallastegui@gmail.com>
2026-01-30 13:20:03 -0300

    Update view3d.js

--- src/view3d.js
+++ src/view3d.js
@@ -2,7 +2,6 @@
 import * as THREE from "https://unpkg.com/three@0.161.0/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.161.0/examples/jsm/controls/OrbitControls.js";
 
-
 /*
   View3D + Editor de waypoints (MVP)
   - Piso con textura del plano (layout.planImage)
